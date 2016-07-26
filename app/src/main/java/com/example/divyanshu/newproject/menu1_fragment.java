d72122c61678c9f6f75cc2416ef522c9b8cc291f
Divyanshu-Grover <divyanshu.grover@gmail.com>
2016-07-27 01:36:34 +0530

    upload commit

---
+++ app/src/main/java/com/example/divyanshu/newproject/menu1_fragment.java
@@ -0,0 +1,20 @@
+package com.example.divyanshu.newproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Divyanshu on 2/7/2015.
+ */
+public class menu1_fragment extends Fragment {
+    View rootview;
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu1_layout,container,false);
+        return rootview;
+    }
+}
